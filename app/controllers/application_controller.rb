b21065a75b10477c28e1912cf755e36dffe4bd81
PemaL <pema.srp72@gmail.com>
2021-12-06 17:25:22 -0500

    added column to users table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,5 +26,12 @@ class ApplicationController < Sinatra::Base
     user.to_json
   end
    
+  # delete '/messages/:id' do
+  #   rm_message= Message.find(params[:id])
+  #   rm_message.destroy
+  #   rm_message.to_json
+  # end
+  
+
   
 end
