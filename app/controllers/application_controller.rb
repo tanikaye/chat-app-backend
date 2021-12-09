2657454328963515ff0337380b0d853974835474
Maurice Smith <Xmaurice.smith@gmail.com>
2021-12-08 20:52:36 -0500

    updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,16 +21,27 @@ class ApplicationController < Sinatra::Base
     user=Conversation.all
     user.to_json
   end
+
   get "/messages" do
     user=Message.all
     user.to_json
   end
 
-  # delete '/messages/:id' do
-  #   rm_message= Message.find(params[:id])
-  #   rm_message.destroy
-  #   rm_message.to_json
+  get "/conversations/:id/messages" do
+    msg=Conversation.find(params[:id]).messages
+    msg.to_json
+  end
+  # get "/conversations/" do
+  #   msg=Conversation.a
+  #   msg.to_json
   # end
+
+  delete '/users/:id' do
+    rm_user= Users.find(params[:id])
+    rm_user.destroy
+    rm_user.to_json
+  end
+
   post '/users' do 
     new_user = User.create(
       username: params[:username],
@@ -39,5 +50,19 @@ class ApplicationController < Sinatra::Base
   )
     new_user.to_json
   end 
+
+
+  post '/messages' do 
+     new_message = Message.create(
+     message: params[:message],
+     user_id: params[:user_id],
+     conversation_id: params[:conversation_id]
+     )
+     new_message.to_json
 end
-#   post '/
+
+
+end 
+
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
 require 'faker'
 puts "🌱 Seeding spices..."
+User.destroy_all
+Conversation.destroy_all
+Message.destroy_all
 
 # Seed your database here
 # luis=User.create({username: "luis", full_name: "Luis Escobar", email: "lae311@nyu.edu"})
@@ -7,5 +10,20 @@ puts "🌱 Seeding spices..."
 # Message.create(message: "I hate myself", user_id: luis.id, conversation_id: uno.id)
 
 10.times do
-    User.create({username: Faker::Name.name, full_name: Faker::Name.name, email: Faker::Internet.email})
+    User.create( 
+        username: Faker::Name.name, 
+        full_name: Faker::Name.name, 
+        email: Faker::Internet.email)
+end
+
+5.times do 
+    Conversation.create(
+        topic: Faker::ProgrammingLanguage.name)
+end
+
+5.times do
+    Message.create(
+        message: Faker::Quotes::Shakespeare.as_you_like_it_quote, 
+        user_id: rand(33..43), 
+        conversation_id: rand(8..13))
 end
